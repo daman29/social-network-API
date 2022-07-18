d411edb0da23ffff4f1f90866334be6240ed4c03
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-07-18 11:58:24 +0930

    added get all users and get single user to user controller

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // get single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
