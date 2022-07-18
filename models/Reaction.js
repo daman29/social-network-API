a5fec4cdc6a2b8ac16b70e604ebc6d466d44869d
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-07-18 11:39:40 +0930

    added user, thought and reaction schema and user and thought models

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        return date.toDateString();
+      },
+    },
+  },
+  {
+    toJson: {
+      getters: true,
+    },
+  }
+);
+
+module.exports = reactionSchema;
